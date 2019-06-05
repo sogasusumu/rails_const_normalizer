e28d28f634ba635a619bab81433b2cd460d36c7d
曽我進 <soga@yocto-inc.com>
2019-06-05 11:55:32 +0900

    created .

--- lib/rails_const_normalizer.rb
+++ lib/rails_const_normalizer.rb
@@ -1,6 +1,71 @@
-require "rails_const_normalizer/version"
+# frozen_string_literal: true
+
+require 'rails_const_normalizer/version'
+require 'active_support/all'
 
 module RailsConstNormalizer
-  class Error < StandardError; end
-  # Your code goes here...
+  refine String do
+    # @return [String]
+    # @raise RuntimeError if receiver not in ``%w(index show create update delete)``
+    def permit!
+      allowed_actions.delete(self).tap { |s| raise("#{self} is not allow action.") if s.nil? }
+    end
+
+    # @return [Array<String>]
+    def allowed_actions
+      %w[index show create update delete]
+    end
+
+    # @return [String]
+    def to(type, format = nil)
+      send(:normalize).yield_self { |str| format ? str.send(type, format) : str.send(type) }
+    end
+
+    # @return [String]
+    def normalize
+      tr('０-９ａ-ｚＡ-Ｚ', '0-9a-zA-Z').gsub(/　/, ' ').gsub(/\s/, '')
+    end
+
+    # @return [String]
+    def controller(format = nil)
+      [underscore.with_out_suffix.pluralize, 'controller'].join('_')
+                                                          .yield_self { |str| format ? str.send(format) : str }
+    end
+
+    # @return [String]
+    def model(format = nil)
+      singularize.underscore
+                 .yield_self { |str| format ? str.send(format) : str }
+    end
+
+    # @return [String]
+    def resources
+      "resources :#{underscore.with_out_suffix.pluralize}"
+    end
+
+    # @return [String]
+    def with_out_suffix
+      gsub(/_controller/, '')
+    end
+
+    # @return [String]
+    def file_name
+      [self, 'rb'].join('.')
+    end
+
+    # @return [String]
+    def klass
+      camelize
+    end
+
+    # @return [String]
+    def klass_name
+      with_out_suffix.camelize
+    end
+
+    # @return [String]
+    def table
+      tableize
+    end
+  end
 end
--- spec/rails_const_normalizer_spec.rb
+++ spec/rails_const_normalizer_spec.rb
@@ -1,9 +1,166 @@
+require 'spec_helper'
+require 'rails_const_normalizer'
+require 'active_support/all'
+
 RSpec.describe RailsConstNormalizer do
-  it "has a version number" do
-    expect(RailsConstNormalizer::VERSION).not_to be nil
+  using RailsConstNormalizer
+
+  describe :normalize do
+    shared_examples :should_remove do
+      it(:should_remove) { expect(actual).to eq expected }
+    end
+
+    let(:expected) { 'normalized' }
+    let(:method_name) { :normalize }
+    let(:actual) { receiver.send(method_name) }
+
+    context :has_double_byte_char do
+      let(:receiver) { 'ｎormalized' }
+      it_behaves_like :should_remove
+    end
+
+    context :has_double_byte_space do
+      let(:receiver) { '　　no　　　rm　　al　iz　ed　　　' }
+      it_behaves_like :should_remove
+    end
+
+    context :has_tab do
+      let(:receiver) { "\t\t\tno\trm\tal\tiz\t\ted\t\t" }
+      it_behaves_like :should_remove
+    end
+
+    context :has_space do
+      let(:receiver) { ' no  rm al iz ed  ' }
+      it_behaves_like :should_remove
+    end
+
+    context :has_enter do
+      let(:receiver) { "\nn\nor\n\nm\n\n\nalized\n\n" }
+      it_behaves_like :should_remove
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe :to do
+    let(:expected) { 'name' }
+    let(:format) { nil }
+    let(:actual) { receiver.to(type, format) }
+
+    shared_examples :should_return_expected do
+      it { expect(actual).to eq expected }
+    end
+
+    shared_examples :format_variations do
+      let(:base_name) { 'name' }
+      let(:format_method) { :pluralize }
+      let(:receiver) { base_name.send(format_method) }
+      it_behaves_like :should_return_expected
+
+      context :camelize do
+        let(:receiver) { base_name.send(format_method).camelize }
+        it_behaves_like :should_return_expected
+      end
+
+      context :has_suffix do
+        let(:receiver) { [base_name.send(format_method), suffix].reject(&:blank?).join('_') }
+        it_behaves_like :should_return_expected
+
+        context :camelize do
+          let(:receiver) { [base_name.send(format_method), suffix].reject(&:blank?).join('_').camelize }
+          it_behaves_like :should_return_expected
+        end
+      end
+    end
+
+    context :controller do
+      let(:type) { :controller }
+      let(:format) { nil }
+      let(:suffix) { type.to_s }
+      let(:expected) { "names_#{suffix}" }
+      it_behaves_like :format_variations
+
+      context :with_out_suffix do
+        let(:format) { :with_out_suffix }
+        let(:expected) { 'names' }
+        it_behaves_like :format_variations
+      end
+
+      context :file_name do
+        let(:format) { :file_name }
+        let(:expected) { "names_#{suffix}.rb" }
+        it_behaves_like :format_variations
+      end
+
+      context :klass do
+        let(:format) { :klass }
+        let(:expected) { "Names#{suffix.camelize}" }
+        it_behaves_like :format_variations
+      end
+
+      context :klass_name do
+        let(:format) { :klass_name }
+        let(:expected) { 'Names' }
+        it_behaves_like :format_variations
+      end
+    end
+
+    context :model do
+      let(:type) { :model }
+      let(:format) { nil }
+      let(:suffix) { nil }
+      let(:expected) { "name" }
+      it_behaves_like :format_variations
+
+      context :file_name do
+        let(:format) { :file_name }
+        let(:expected) { "name.rb" }
+        it_behaves_like :format_variations
+      end
+
+      context :klass do
+        let(:format) { :klass }
+        let(:expected) { "Name" }
+        it_behaves_like :format_variations
+      end
+
+      context :table do
+        let(:format) { :table }
+        let(:expected) { 'names' }
+        it_behaves_like :format_variations
+      end
+    end
+
+    context :resources do
+      let(:type) { :resources }
+      let(:format) { nil }
+      let(:suffix) { 'controller' }
+      let(:expected) { "resources :names" }
+      it_behaves_like :format_variations
+    end
+  end
+
+  describe :actions do
+    shared_context :should_raise_invalid_value do
+      it { expect { receiver.send(method_name) }.to raise_error(RuntimeError) }
+    end
+
+    shared_context :should_return_expected do
+      let(:expected) { receiver.normalize }
+      it { expect(receiver.send(method_name)).to eq expected }
+    end
+
+    describe :permit do
+      let(:method_name) { :permit! }
+      context :invalid_value do
+        let(:receiver) { 'invalid' }
+        it_behaves_like :should_raise_invalid_value
+      end
+
+      context :valid_value do
+        %w(index show create update delete).each do |action|
+          let(:receiver) { action }
+          it_behaves_like :should_return_expected
+        end
+      end
+    end
   end
 end
